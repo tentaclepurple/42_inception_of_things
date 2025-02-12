1436b851c87df6e8c0632ded58fb7f230c29fcc5
tentaclepurple <wolfframio@gmail.com>
2025-02-12 01:49:43 +0100

    2nd vm

---
+++ p1/scripts/server_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# update
+apt-get update
+apt-get upgrade -y
+
+# InstalL k3s in server mode
+curl -sfL https://get.k3s.io | sh -
+
+# save token for worker
+sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/confs/node-token
+
+# Install kubectl (already comes with k3s but just in case)
+if ! command -v kubectl &> /dev/null; then
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    chmod +x kubectl
+    mv kubectl /usr/local/bin/
+fi
+
+# Config no password ssh
+if [ ! -f /home/vagrant/.ssh/id_rsa ]; then
+    sudo -u vagrant ssh-keygen -t rsa -N "" -f /home/vagrant/.ssh/id_rsa
+fi
