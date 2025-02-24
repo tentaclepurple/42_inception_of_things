abe192fa80c2a77a5abf73af4d08df6d666b02e2
tentaclepurple <wolfframio@gmail.com>
2025-02-24 20:53:06 +0100

    p1 working

--- p1/config/server.sh
+++ p1/config/server.sh
@@ -3,6 +3,6 @@
 apk add curl
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --node-ip $1 --bind-address=$1" sh -s -
 
-sleep 15
+sleep 18
 
 cp /var/lib/rancher/k3s/server/node-token /vagrant/.
