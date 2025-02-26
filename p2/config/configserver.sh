8a30458723fc29b6ad9757955c1b6659dda197d4
tentaclepurple <wolfframio@gmail.com>
2025-02-26 11:51:31 +0100

    wo p2

---
+++ p2/config/configserver.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --node-ip $1 --bind-address=$1" sh -s -
+
+sleep 18
+
+kubectl apply -n kube-system -f /vagrant/apps/app-1.yml --validate=false
+kubectl apply -n kube-system -f /vagrant/apps/app-2.yml --validate=false
+kubectl apply -n kube-system -f /vagrant/apps/app-3.yml --validate=false
+kubectl apply -n kube-system -f /vagrant/apps/ingress.yaml --validate=false
