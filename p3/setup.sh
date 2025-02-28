255f2768a92a39bcc3d4f34992319a4f379173ab
johnconh <sevenballs@gmail.com>
2025-02-28 19:40:42 +0100

    mejor

--- p3/setup.sh
+++ p3/setup.sh
@@ -9,8 +9,8 @@ kubectl create namespace dev
 
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-sleep 60
 echo "Waiting for ArgoCD to start..."
+sleep 60
 
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d > argopass
 
